79cc6932b37330ab2d666a851319fbb990542209
PepeMola <pepe.rocks9@gmail.com>
2020-11-04 18:47:43 +0100

    Class: Node --Version 1.0

---
+++ src/Node.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -- coding: utf-8 --
+
+class Node:
+    def __init__(self, idNode, cost, idState, idParent, action, depth, heuristic, value):
+        self.idNode
+        self.cost
+        self.idState
+        self.idParent
+        self.action
+        self.depth
+        self.heuristic
+        self.value
