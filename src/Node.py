ad89601a4b7f407a1c74e402b49a7c99c8f2440e
PepeMola <pepe.rocks9@gmail.com>
2020-11-04 19:25:06 +0100

    Class: Node already developed. --Version: 1.1

--- src/Node.py
+++ src/Node.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 # -- coding: utf-8 --
 
+import Maze, Cell
 class Node:
     def __init__(self, idNode, cost, idState, idParent, action, depth, heuristic, value):
-        self.idNode
-        self.cost
-        self.idState
-        self.idParent
-        self.action
-        self.depth
-        self.heuristic
-        self.value
+        self.idNode = idNode #It will increase with the creation of new nodes (starts in 0)
+        self.cost = cost #Accumulated cost by the path to that node
+        self.idState = idState #Access to the space state in that node
+        self.idParent = idParent #Access to the parent node
+        self.action = action #Movement that has generated the node
+        self.depth = depth #Level of the node (root node = 0)
+        self.heuristic = heuristic #Value of the heuristic
+        self.value = value #Value of the node according to the selected strategy
+
+    
+'''
+The String which must be generated is:
+    [<idNode>] [<cost>, <idState>, <idParent>, <action>, <depth>, <heuristic>, <value>]
+'''
