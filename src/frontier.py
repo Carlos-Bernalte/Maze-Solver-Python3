2b3afcdb4e40e8e25e444f6d3697dc5553596326
Angel Custodio <angelglvz99@gmail.com>
2020-11-05 20:32:01 +0100

    intento de frontier 1

---
+++ src/frontier.py
@@ -0,0 +1,34 @@
+class Frontier:
+    def __init__(self):
+        self.frontier=[]
+
+    def removeNode(self):
+        if (self.frontier):
+            nodeList = self.frontier.pop(0)
+            return nodeList
+        else:
+            return None
+
+    def isEmpty(self):
+        if (self.frontier):
+            return False
+        else:
+            return True
+
+    def insertLis(self, nodeList):
+        if(nodeList is not None):
+            for node in nodeList:
+                self.inserNode(node)
+
+
+    def insertNode(self, node):
+        if (len(self.frontier)==0):
+            self.frontier.append(node)
+        else:
+            x=0
+            while x< len(self.frontier):
+                if (self.frontier[x].idState.equals(node.idState)):
+                    break
+                x+=1
+            if x==len(self.frontier):
+                self.frontier.insert(len(self.frontier),node)
